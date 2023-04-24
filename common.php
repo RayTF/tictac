a4c1de4e95daf82323119c07f2c54583b9c1c57a
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-04-24 11:17:58 -0400

    Update common.php

--- common.php
+++ common.php
@@ -3,7 +3,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-		<link rel="stylesheet" id="bootstrap" href="sb-finalium.css" type="text/css">
+		<link rel="stylesheet" id="bootstrap" href="classic.css" type="text/css">
 		<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
